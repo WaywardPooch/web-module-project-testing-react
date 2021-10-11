215ece95a5971762b0334729736062d93b5eeed9
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-11 13:10:30 -0700

    completed the episode tests

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
 const testEpisode = {
-    id:1,
-    name: "",
-    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    season: 1,
-    number: 1,
-    summary: "",
-    runtime: 1
-}
+  id: 1,
+  name: "",
+  image:
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+  season: 1,
+  number: 1,
+  summary: "Welcome to the Aperture Science computer-aided enrichment center.",
+  runtime: 1,
+};
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+  id: 2,
+  name: "Test Episode w/o image",
+  image: null,
+  season: 1,
+  number: 1,
+  summary: "",
+  runtime: 1,
+};
 
 test("renders without error", () => {
-
+  render(<Episode episode={testEpisode} />);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", () => {
+  // Arrange
+  render(<Episode episode={testEpisode} />);
+
+  // Act
+  const summary = screen.queryByText(
+    /Welcome to the Aperture Science computer-aided enrichment center./i
+  );
+
+  // Assert
+  expect(summary).toBeInTheDocument();
+  expect(summary).not.toBeNull();
+  expect(summary).toBeTruthy();
 });
 
-test("renders default image when image is not defined", ()=>{
-    
-})
+test("renders default image when image is not defined", () => {
+  // Arrange
+  render(<Episode episode={testEpisodeWithoutImage} />);
+
+  // Act
+  const altTag = screen.queryByAltText("./stranger_things.png");
+
+  // Assert
+  expect(altTag).toBeInTheDocument();
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
