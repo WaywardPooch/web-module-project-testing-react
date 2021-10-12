0eaa7ba277576ec72637acf534174fb6d05dd7c5
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-11 17:19:02 -0700

    working on final 2 Display component tests

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,68 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+import Display from "./../Display";
 
+import fetchShow from "./../../api/fetchShow";
+jest.mock("./../../api/fetchShow");
 
+const testShow = {
+  name: "Half Life 2",
+  summary:
+    "The right man in the wrong place can make all the difference in the world.",
+  seasons: [
+    {
+      id: 1,
+      name: "Season 1",
+      episodes: ["Empire Strikes Back", "Fun Times", "Brady Bunch"],
+    },
+    {
+      id: 2,
+      name: "Season 2",
+      episodes: ["Empire Strikes Back", "Fun Times", "Brady Bunch"],
+    },
+    {
+      id: 3,
+      name: "Season 3",
+      episodes: ["Empire Strikes Back", "Fun Times", "Brady Bunch"],
+    },
+  ],
+};
 
+test("Display renders without errors", () => {
+  render(<Display />);
+});
 
+test("When fetch button is pressed, show component displays", async () => {
+  // Arrange
+  render(<Display />);
 
+  fetchShow.mockResolvedValueOnce({
+    data: testShow,
+  });
+  // Act
+  const fetchButton = screen.queryByRole("button");
+  userEvent.click(fetchButton);
+  // Assert
+  const showComponent = await screen.findByTestId("show-container");
+  expect(showComponent).toBeInTheDocument();
+});
 
+test("When fetch button is pressed, options count matches season count", async () => {
+  // Arrange
+  render(<Display />);
 
-
-
-
-
-
-
+  fetchShow.mockResolvedValueOnce({
+    data: testShow,
+  });
+  // Act
+  const fetchButton = screen.queryByRole("button");
+  userEvent.click(fetchButton);
+  // Assert
+  const seasonsDropdown = await screen.findByRole("combobox");
+  expect(seasonsDropdown).toHaveLength(testShow.seasons.length + 1);
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
